98a9e77375d74d508fc0ad13a65364f779636cdb
Odalis Pineda <odalispineda70@gmail.com>
2022-02-16 23:40:26 -0600

    subiendo tarea

--- Ejercicios/Ejercicio25 - SumaDeParesImparesTotal_Tarea#4/pares2.cpp
+++ Ejercicios/Ejercicio25 - SumaDeParesImparesTotal_Tarea#4/pares2.cpp
@@ -4,19 +4,29 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int pares = 0;
+    int pares = 0, impar = 0, total = 0;
 
     for (int i = 1; i <= 10; i++)
     {
         if (i % 2 == 0) {
             pares = pares + i;
+        } else
+        {
+            if ( i % 1 == 0)
+            {
+                impar = impar + i;
+            }
+            
         }
+        total = pares + impar;
         cout << i << " ";
     }
 
     cout << endl;
     cout << endl;
-    cout << "Total pares: " << pares;
+    cout << "Total pares: " << pares << endl;
+    cout << "Total impares: " << impar << endl;
+    cout << "Suma Total: " << total << endl;
 
     return 0;
 }
