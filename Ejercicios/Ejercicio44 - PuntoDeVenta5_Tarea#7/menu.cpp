125805aac888ff740d1f8dcbd651673b9817eec1
Odalis Pineda <odalispineda70@gmail.com>
2022-03-16 23:24:26 -0600

    agg tarea 7

---
+++ Ejercicios/Ejercicio44 - PuntoDeVenta5_Tarea#7/menu.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+extern void productos(int opcion);
+extern void imprimirFactura();
+
+void menu()
+{
+    int opcion = 0;
+
+    while(true)
+    {
+        system("cls");
+        
+        cout << "****";
+        cout << "MENU";
+        cout << "****";
+        cout << endl;
+        cout << endl;
+        cout << endl;
+
+        cout << "1 - Bebidas Calientes" << endl;
+        cout << "2 - Bebidas Frias" << endl;
+        cout << "3 - Reposteria" << endl;
+        cout << "4 - Imprimir Factura" << endl;
+        cout << "0 - Salir" << endl;
+
+        cout << endl;
+        cout << "Ingrese una opcion: ";
+        cin >> opcion;   
+
+        if (opcion == 0) 
+        {
+            cout << "<< Adiosito, vuelva pronto, vales mil >>" <<endl;
+            break;
+        }    
+        if (opcion == 4)
+        {
+            imprimirFactura();
+        } else {
+            productos(opcion);
+        }
+    }
+}
